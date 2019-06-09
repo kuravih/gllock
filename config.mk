b7548cf91a3eccbdf66e7702241f1047d8a4c724
Aaron Moore <aaron@atamisk.net>
2019-06-09 09:04:12 -0400

    Removed symlink to user's home directory.

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 0.1-alpha
 # Customize below to fit your system
 
 # paths
-SHADER_LOCATION = $(HOME)/.gllock
+SHADER_LOCATION = /usr/lib/gllock/shaders/
 
 # shader
 # FRGMNT_SHADER = blur.fragment.glsl
@@ -14,7 +14,7 @@ FRGMNT_SHADER = circle.fragment.glsl
 # FRGMNT_SHADER = ascii.fragment.glsl
 # FRGMNT_SHADER = crt.fragment.glsl
 
-PREFIX = /usr/local
+PREFIX = /usr
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
